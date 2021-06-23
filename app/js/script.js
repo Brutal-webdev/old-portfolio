0f4385e17cf36a03726fc1bdb39a1ae0b03d3f05
Dominic Scaccia <dominic.777@live.com>
2021-06-23 15:16:47 -0700

    added javascript for animation overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger');
+
+    if(header.classList.contains('open')){ // close hamburger menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else { // open hamburger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});

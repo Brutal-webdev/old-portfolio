3def70ef4640c52bbc91bb0066f75f2598c4b9b4
Dominic Scaccia <dominic.777@live.com>
2021-06-23 15:30:44 -0700

    added touch functionality to menu

--- app/js/script.js
+++ app/js/script.js
@@ -2,22 +2,7 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 
-btnHamburger.addEventListener('click', function(){
-    console.log('open hamburger');
-
-    if(header.classList.contains('open')){ // close hamburger menu
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-    }
-    else { // open hamburger menu
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
-    }
-});
-
-btnHamburger.addEventListener('touchstart', function(){
+btnHamburger.addEventListener('click touch', function(){
     console.log('open hamburger');
 
     if(header.classList.contains('open')){ // close hamburger menu

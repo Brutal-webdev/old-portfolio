61f87b11a4a384e336b130205273afdd81a8702b
Dominic Scaccia <dominic.777@live.com>
2021-06-23 19:08:27 -0700

    added mobile menu popup

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,27 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
     console.log('open hamburger');
 
     if(header.classList.contains('open')){ // close hamburger menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }
     else { // open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     }
 });
 
@@ -21,13 +29,21 @@ btnHamburger.addEventListener('touch', function(){
     console.log('open hamburger');
 
     if(header.classList.contains('open')){ // close hamburger menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }
     else { // open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     }
 });
+
+
